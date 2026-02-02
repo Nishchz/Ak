52eb17d001da35c75655e031f11caf5c7dd9fff1
Nishchz <anishchz01@gmail.com>
2026-02-02 23:56:12 +0530

    Initial commit

---
+++ LinkedList.java
@@ -0,0 +1,124 @@
+public class LinkedList {
+
+    // Node class
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static Node head;
+    static Node tail;
+    static int size;
+
+    // Add First
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Add Last
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    //  Remove First
+    public int removeFirst() {
+        if (size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }
+
+        int val = head.data;
+
+        if (size == 1) {
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    //  Remove Last
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }
+
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+
+        Node prev = head;
+
+        // prev will reach size-2 index
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+
+        int val = tail.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+
+        return val;
+    }
+
+    // Print Linked List
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // Main
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+
+        // 1 -> 2 -> 3 -> 4 -> 5 -> null
+        ll.print();
+
+        ll.removeFirst(); // removes 1
+        ll.print();       // 2 -> 3 -> 4 -> 5 -> null
+
+        ll.removeLast();  // removes 5
+        ll.print();       // 2 -> 3 -> 4 -> null
+    }
+}
